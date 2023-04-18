333d57e8ed3ed8d3a69a8569d54d9a4263d95c73
David Katende <katendedavidjoga@gmail.com>
2023-04-18 11:14:45 +0300

    Ads blur background

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
 document.addEventListener("DOMContentLoaded",function(){
 
-    let humbugerDiv = document.querySelector('#humbuger');
-    let navMenu = document.querySelector('.nav-menu');
+    let humbugerDiv = document.querySelector('.humbuger');
+    let navMenu = document.querySelector('.nav-container');
+    let navBtn = document.querySelector('.btn-close');
+    let navUl = document.querySelector('.nav-menu');
 
     humbugerDiv.addEventListener('click',function() {
        // $('.navMenu').fadeIn('500');
-        navMenu.classList.toggle('active');
-
-        let navBtn = document.querySelector('.btn-close');
-        navBtn.classList.toggle('btn-close-show');   
-
-       /// div.classList.add('.active');
-
+       
+      navUl.classList.toggle('nav-menu');
+     navUl.classList.add('nav-menu-white');
+      navBtn.classList.add('btn-close-show');   
+      navMenu.classList.remove('nav-container'); 
+      navMenu.classList.add('nav-container-cover');
+      humbugerDiv.classList.toggle('humbuger-hide');     
+     
     });
+
+   
+
+
 
 });
