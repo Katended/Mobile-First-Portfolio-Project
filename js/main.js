86563992314efd9f724b59430472f40b89f551dc
David Katende <katendedavidjoga@gmail.com>
2023-04-18 15:38:24 +0300

    Fix overlay

--- js/main.js
+++ js/main.js
@@ -1,31 +1,29 @@
-document.addEventListener("DOMContentLoaded",function(){
+document.addEventListener('DOMContentLoaded', () => {
+  const humbugerDiv = document.querySelector('.humbuger');
+  const navMenu = document.querySelector('.nav-container');
+  const navBtn = document.querySelector('.btn-close');
+  const navUl = document.querySelector('.nav-menu');
+  const MenuItems = document.querySelectorAll('.nav-menu');
 
-    let humbugerDiv = document.querySelector('.humbuger');
-    let navMenu = document.querySelector('.nav-container');
-    let navBtn = document.querySelector('.btn-close');
-    let navUl = document.querySelector('.nav-menu');
-
-    humbugerDiv.addEventListener('click',function() {
-       // $('.navMenu').fadeIn('500');
-       
-      navUl.classList.toggle('nav-menu');
-     navUl.classList.add('nav-menu-white');
-      navBtn.classList.add('btn-close-show');   
-      navMenu.classList.remove('nav-container'); 
-      navMenu.classList.add('nav-container-cover');
-      humbugerDiv.classList.toggle('humbuger-hide');     
-     
+  humbugerDiv.addEventListener('click', () => {
+    navUl.classList.toggle('nav-menu');
+    navUl.classList.add('nav-menu-white');
+    navBtn.classList.add('btn-close-show');
+    navMenu.classList.remove('nav-container');
+    navMenu.classList.add('nav-container-cover');
+    humbugerDiv.classList.toggle('humbuger-hide');
+  });
+  navBtn.addEventListener('click', () => {
+    navMenu.classList.add('nav-container');
+    navBtn.classList.toggle('btn-close-show');
+    humbugerDiv.classList.remove('humbuger-hide');
+    navMenu.classList.toggle('nav-container');
+    navUl.classList.toggle('nav-menu');
+    navMenu.classList.toggle('nav-container-cover');
+  });
+  MenuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+      navBtn.click();
     });
-
-    navBtn.addEventListener('click',function() {
-      navMenu.classList.add('nav-container'); 
-       navBtn.classList.toggle('btn-close-show');   
-       humbugerDiv.classList.remove('humbuger-hide');  
-       navMenu.classList.toggle('nav-container');
-       navUl.classList.toggle('nav-menu'); 
-       navMenu.classList.toggle('nav-container-cover');     
-    });
-
-
-
+  });
 });
