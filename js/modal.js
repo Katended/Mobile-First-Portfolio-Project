5ccc2fb364939abd1ac4dc97aad56c1ee2822db6
David Katende <katendedavidjoga@gmail.com>
2023-04-20 15:56:50 +0300

    Fix popup size and layout

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,5 @@
 const bodyTag = document.querySelector('body');
 const worksContainter = document.getElementById('portfolio');
-
 const defaultWorks = [
   {
     title: 'Tonic',
@@ -9,11 +8,10 @@ const defaultWorks = [
     imgdesk: './images/snapshoot-portfolio-desktop1.png',
     imgmob: './images/snapshoot-portfolio-mobile1.png',
     employment: ['Back End Dev', '2015'],
-    technologies: ['HTML', 'CCS', 'JavaScript'],
+    technologies: ['HTML', 'CCS', 'JavaScript', 'Ruby', 'Bootstrap'],
     dataset: 0,
   },
   {
-  
     title: 'Multi-Post Stories',
     subtitle: 'FACEBOOK',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
@@ -24,7 +22,6 @@ const defaultWorks = [
     dataset: 1,
   },
   {
-    
     title: 'Tonic',
     subtitle: 'FACEBOOK',
     description: "A daily selection of privately personalized reads; no accounts or sign-ups required. Has been the industry's standard.Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
@@ -43,20 +40,18 @@ const defaultWorks = [
     employment: ['Lead Developer', '2018'],
     technologies: ['HTML', 'Bootstrap', 'Ruby'],
     dataset: 3,
-  },  
+  },
 ];
 
 // function to add element given tag name and class name
 const createElement = (tag, className) => {
   const el = document.createElement(tag);
-  
-  if(typeof className !== 'undefined')
-  el.classList.add(className);
-
+  if (typeof className !== 'undefined') { el.classList.add(className); }
   return el;
 };
 
 // function to create ul and li elements given an array of items & the class name of the ul element
+
 const createList = (items, className) => {
   const ul = createElement('ul', className);
   items.forEach((item) => {
@@ -77,162 +72,129 @@ const createButton = (className, dataset, text) => {
 
 // function expression for add other projects
 const createDefaultWorks = (defaultWorks) => {
-
   // create card container
   const cardContainter = createElement('div', 'card-one');
-  
   // create image div
   const imgDiv = createElement('div');
   cardContainter.appendChild(imgDiv);
-
   // create image
   const imgDesk = createElement('img');
   imgDesk.src = defaultWorks.imgdesk;
-  imgDesk.classList.add('imgalin', 'imgdesk'+parseInt(defaultWorks.dataset+1));
+  imgDesk.classList.add('imgalin', `imgdesk${parseInt(defaultWorks.dataset + 1, 10)}`);
   imgDiv.appendChild(imgDesk);
-
   const imgMob = createElement('img');
   imgMob.src = defaultWorks.imgmob;
-  imgMob.classList.add('imgalin', 'imgmob'+parseInt(defaultWorks.dataset+1));
+  imgMob.classList.add('imgalin', `imgmob${parseInt(defaultWorks.dataset + 1, 10)}`);
   imgDiv.appendChild(imgMob);
-
   // create image div
-  const subSectionDiv = createElement('div','grid-element');
+  const subSectionDiv = createElement('div', 'grid-element');
   cardContainter.appendChild(subSectionDiv);
-
   // Section title
   const defaultWorksTitle = createElement('h2');
   defaultWorksTitle.innerHTML = defaultWorks.title;
   subSectionDiv.appendChild(defaultWorksTitle);
-
   // Section span
-  const subSectionSpan1 = createElement('span','sublinks'); 
+  const subSectionSpan1 = createElement('span', 'sublinks');
   subSectionDiv.appendChild(subSectionSpan1);
-
   // Section span title
-  const subSectionSpan2 = createElement('span'); 
+  const subSectionSpan2 = createElement('span');
   subSectionSpan2.innerHTML = defaultWorks.subtitle;
   subSectionSpan1.appendChild(subSectionSpan2);
-
   const employmentHistory = createList(defaultWorks.employment);
   subSectionSpan1.appendChild(employmentHistory);
- 
-  //description
-  const descriptionTag = createElement('p'); 
+  // description
+  const descriptionTag = createElement('p');
   descriptionTag.innerHTML = defaultWorks.description;
   subSectionDiv.appendChild(descriptionTag);
-
   // Section span tech
-  const sectionTechSpan = createElement('span','sublinksround'); 
+  const sectionTechSpan = createElement('span', 'sublinksround');
   subSectionDiv.appendChild(sectionTechSpan);
-
   // list of technologies
   const technologiesList = createList(defaultWorks.technologies);
   sectionTechSpan.appendChild(technologiesList);
-  
   // 'see project' button
   const seeProjectBtn = createButton('project-btn', defaultWorks.dataset, 'See Project');
   seeProjectBtn.classList.add('call-to-action');
   subSectionDiv.appendChild(seeProjectBtn);
-
   return cardContainter;
 };
 
 defaultWorks.forEach((item) => {
-   worksContainter.appendChild(createDefaultWorks(item));
+  worksContainter.appendChild(createDefaultWorks(item));
 });
 
-
 function windowModal(e) {
-
-    // create offscreen DOM tree.
   const docFrag = document.createDocumentFragment();
   const projectCardInfo = defaultWorks[parseInt(e.target.dataset.projectbtn, 10)];
-  console.log(e.target.dataset.projectbtn);
-  const projectPopup = document.createElement('div','projectPopup');
-
+  const projectPopup = document.createElement('div', 'projectPopup');
   const cardContainter = createElement('div', 'popupContainer');
-
-  // Section title
-  const defaultWorksTitle = createElement('h2', 'popUp');
-  defaultWorksTitle.innerHTML = projectCardInfo.title;
-  cardContainter.appendChild(defaultWorksTitle);
-
-  const closeIcon = createElement('span','closePop'); 
+  const closeIcon = createElement('span', 'closePop');
   closeIcon.innerHTML = '&times;';
   closeIcon.id = 'closeBtn';
   cardContainter.appendChild(closeIcon);
-
-  // Section span
-  const subSectionSpan1 = createElement('span','sublinks'); 
+  const subSectionSpan1 = createElement('span', 'sublinks');
+  subSectionSpan1.id = 'item4';
   cardContainter.appendChild(subSectionSpan1);
-
-   // Section span title
-   const subSectionSpan2 = createElement('span','subtittle'); 
-   subSectionSpan2.innerHTML = projectCardInfo.subtitle;
-   subSectionSpan1.appendChild(subSectionSpan2);
-
+  const defaultWorksTitle = createElement('h2', 'popUp');
+  defaultWorksTitle.innerHTML = projectCardInfo.title;
+  subSectionSpan1.appendChild(defaultWorksTitle);
+  const subSectionSpan2 = createElement('span', 'subtittle');
+  subSectionSpan2.innerHTML = projectCardInfo.subtitle;
+  subSectionSpan1.appendChild(subSectionSpan2);
   const employmentHistory = createList(projectCardInfo.employment);
   subSectionSpan1.appendChild(employmentHistory);
-
-
-  const imgDiv = createElement('div');
+  const imgDiv = createElement('div', 'item2');
   cardContainter.appendChild(imgDiv);
 
   // create image
   const imgDesk = createElement('img');
   imgDesk.src = projectCardInfo.imgdesk;
-  imgDesk.classList.add('imgalin', 'imgdesk'+parseInt(projectCardInfo.dataset+1));
+  imgDesk.classList.add('imgalin', `imgdesk${parseInt(projectCardInfo.dataset + 1, 10)}`);
   imgDiv.appendChild(imgDesk);
-
   const imgMob = createElement('img');
   imgMob.src = projectCardInfo.imgmob;
-  imgMob.classList.add('imgalin', 'imgmob'+parseInt(projectCardInfo.dataset+1));
+  imgMob.classList.add('imgalin', `imgmob${parseInt(projectCardInfo.dataset + 1, 10)}`);
   imgDiv.appendChild(imgMob);
-
-  // create image div
-  const subSectionDiv = createElement('div','grid-element');
+  const subSectionDiv = createElement('div', 'grid-element');
   cardContainter.appendChild(subSectionDiv);
-  
-  //description
-  const descriptionTag = createElement('p'); 
+  // surround div
+  const surroundDiv = createElement('div', 'bottomflex');
+  subSectionDiv.appendChild(surroundDiv);
+  // description
+  const descriptionTag = createElement('p');
   descriptionTag.innerHTML = projectCardInfo.description;
-  subSectionDiv.appendChild(descriptionTag);
-
+  surroundDiv.appendChild(descriptionTag);
+  // link div
+  const linkDiv = createElement('div', 'linkdiv');
+  surroundDiv.appendChild(linkDiv);
   // Section span tech
-  const sectionTechSpan = createElement('span','sublinksround'); 
-  subSectionDiv.appendChild(sectionTechSpan);
-
+  const sectionTechSpan = createElement('span', 'sublinksround');
+  linkDiv.appendChild(sectionTechSpan);
   // list of technologies
   const technologiesList = createList(projectCardInfo.technologies);
   sectionTechSpan.appendChild(technologiesList);
-
-  const popupLinkContainer = createElement('div', 'popupLinkContainer');
-  subSectionDiv.appendChild(popupLinkContainer);
-
+  const popupLinkContainer = createElement('span', 'popupLinkContainer');
+  linkDiv.appendChild(popupLinkContainer);
   const liveLink = createElement('a', 'liveLink');
   liveLink.innerHTML = 'See live&nbsp;&nbsp;<img  src="/images/icons/live.png" size="50%">';
   liveLink.href = 'https://katended.github.io/mobilefirstproject2/Index.html';
   popupLinkContainer.appendChild(liveLink);
-
   const sourceLink = createElement('a', 'sourceLink');
   sourceLink.innerHTML = 'See source&nbsp;&nbsp;<img  src="/images/icons/git.png" class="btnIcons">';
   sourceLink.href = 'https://katended.github.io/mobilefirstproject2/';
   popupLinkContainer.appendChild(sourceLink);
-  
+  docFrag.appendChild(cardContainter);
   projectPopup.appendChild(docFrag);
-  cardContainter.appendChild(projectPopup);
-  bodyTag.appendChild(cardContainter);
-
+  bodyTag.appendChild(projectPopup);
   document.querySelector('header').style.filter = 'blur(10px)';
   document.getElementById('about-me').style.filter = 'blur(10px)';
   document.getElementById('intro').style.filter = 'blur(10px)';
   document.getElementById('portfolio').style.filter = 'blur(10px)';
   document.getElementById('contact').style.filter = 'blur(10px)';
-  
-   const closeBtn = document.getElementById('closeBtn');
+
+  const closeBtn = document.getElementById('closeBtn');
   closeBtn.addEventListener('click', () => {
-    bodyTag.removeChild(cardContainter);
+    bodyTag.removeChild(projectPopup);
     document.querySelector('header').style.filter = 'blur(0px)';
     document.getElementById('about-me').style.filter = 'blur(0px)';
     document.getElementById('intro').style.filter = 'blur(0px)';
