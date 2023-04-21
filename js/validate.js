1af9f2a4f9aea3d9db4e6f6a4ea51c13aceba33b
David Katende <katendedavidjoga@gmail.com>
2023-04-21 22:47:51 +0300

    Fix linters

--- js/validate.js
+++ js/validate.js
@@ -1,13 +1,11 @@
 const form = document.getElementById('myform');
-form.addEventListener('submit', (event) => {        
-    const email = document.getElementById('email').value;
-  
-    if(/[A-Z]/.test(email)){
-
-        const errorMessage = document.querySelector('.errorMessage');  
-        errorMessage.style.display='block';
-        event.preventDefault();  
-        errorMessage.textContent = "Invalid email. Please use lowercase letters.";                 
-    }   
-    errorMessage.style.display='none'; 
-  }); 
+form.addEventListener('submit', (event) => {
+  const email = document.getElementById('email').value;
+  const errorMessage = document.querySelector('.errorMessage');
+  if (/[A-Z]/.test(email)) {
+    errorMessage.style.display = 'block';
+    event.preventDefault();
+    errorMessage.textContent = 'Invalid email. Please use lowercase letters.';
+  }
+  errorMessage.style.display = 'none';
+});
