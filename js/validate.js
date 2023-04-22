13a69075d45892891c2dfc5c2a6a1cda1ee540e2
David Katende <katendedavidjoga@gmail.com>
2023-04-22 11:12:20 +0300

    Fix linter error

--- js/validate.js
+++ js/validate.js
@@ -1,12 +1,12 @@
 const form = document.getElementById('myform');
-form.addEventListener('submit', (event) => {  
+form.addEventListener('submit', (event) => {
   const email = document.getElementById('email').value;
-  const errorMessage = document.querySelector('.errorMessage');  
+  const errorMessage = document.querySelector('.errorMessage');
   if (/[A-Z]/.test(email)) {
-    errorMessage.style.display = 'block'; 
+    errorMessage.style.display = 'block';
     event.preventDefault();
     errorMessage.textContent = 'Invalid email. Please use lowercase letters.';
-  }else {
+  } else {
     errorMessage.style.display = 'none';
   }
 });
